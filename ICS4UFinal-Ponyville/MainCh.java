c6082141412d1241ac3776e5ed67f71160d09268
349225219 <101135430+349225219@users.noreply.github.com>
2023-05-31 11:57:28 -0400

    added move method for main character and picture of maint character

--- ICS4UFinal-Ponyville/MainCh.java
+++ ICS4UFinal-Ponyville/MainCh.java
@@ -16,4 +16,21 @@ public class MainCh extends SuperSmoothMover
     {
         // Add your action code here.
     }
+    
+    public void move(){
+        int x = getX(), y = getY();
+        if(Greenfoot.isKeyDown("w")){
+            y -= 2;
+        }
+        if(Greenfoot.isKeyDown("a")){
+            x -= 2;
+        }
+        if(Greenfoot.isKeyDown("s")){
+            y += 2;
+        }
+        if(Greenfoot.isKeyDown("d")){
+            x += 2;
+        }
+        setLocation(x,y);
+    }
 }
