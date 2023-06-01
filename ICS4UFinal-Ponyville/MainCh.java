980ee0066ece0d67d0b7e686d862d0d0ef99f6f3
Silver267 <jxuanxi@gmail.com>
2023-06-01 12:08:41 -0400

    Daily Dev

--- ICS4UFinal-Ponyville/MainCh.java
+++ ICS4UFinal-Ponyville/MainCh.java
@@ -3,35 +3,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MainCh here.
  * 
- * @author (your name) 
+ * @author Xuanxi Jiang
  * @version (a version number or a date)
  */
-public class MainCh extends SuperSmoothMover
-{
+public class MainCh extends SuperSmoothMover{
     /**
      * Act - do whatever the MainCh wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act(){
         this.setImage("TheHeart.png");
         move();
     }
     
     public void move(){
         int x = getX(), y = getY();
+        int[] gridPos = ((MainWorld)getWorld()).getMap().getMaps(new int[]{x, y});
         if(Greenfoot.isKeyDown("w")){
-            y -= 2;
+            if(gridPos[1]-1>=0)
+                gridPos[1]--;
         }
         if(Greenfoot.isKeyDown("a")){
-            x -= 2;
+            if(gridPos[0]-1>=0)
+                gridPos[0]--;
         }
         if(Greenfoot.isKeyDown("s")){
-            y += 2;
+            if(gridPos[1]+1<11)
+                gridPos[1]++;
         }
         if(Greenfoot.isKeyDown("d")){
-            x += 2;
+            if(gridPos[0]+1<20)
+                gridPos[0]++;
         }
-        setLocation(x,y);
+        int[] realPos = ((MainWorld)getWorld()).getMap().getPixes(gridPos);
+        setLocation(realPos[0],realPos[1]);
+        Statics.setPlayerCoords(new int[]{gridPos[0], gridPos[1]});
     }
 }
